b749446e574a6a6058001023f6be88a7eae3f849
Gabriel Villanueva <gvillanuevavega@gmail.com>
2023-12-25 18:41:45 -0500

    finish 2.5

--- part2/src/components/Course.jsx
+++ part2/src/components/Course.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
 
-function Course({course}) {
-    const total = course.parts.reduce((sum, part) => sum + part.exercises, 0)
-  return (
-    <div>
-        <p>{course.name}</p>
-        {
-           course.parts.map(part => <p key={part.id}>{part.name} {part.exercises}</p>) 
-        }
+function Course({courses}) {
+    // const total = course.parts.reduce((sum, part) => sum + part.exercises, 0)
+return (
+   <>
+    {
+     courses.map(course => {
+         return (
+             <div key={course.id}>
+                <h2>{course.name}</h2>
+                <ul>
+                    {course.parts.map(part => <li key={part.id}>{part.name} {part.exercises}</li>)}
+                </ul>
+                <p><strong>total of {course.parts.reduce((sum, part) => sum + part.exercises, 0)} exercises</strong></p>
+                
+             </div>
+         )
+         })
+    }
 
-        <p><strong>total of {total} exercises</strong></p>
-    </div>
+   </>
     
   )
 }
