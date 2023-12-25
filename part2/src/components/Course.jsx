c0f88b21361bca2326c060c9b9e0c00e7829c166
Gabriel Villanueva <gvillanuevavega@gmail.com>
2023-12-25 18:31:51 -0500

    finish 2.2 and 2.3

--- part2/src/components/Course.jsx
+++ part2/src/components/Course.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 
 function Course({course}) {
+    const total = course.parts.reduce((sum, part) => sum + part.exercises, 0)
   return (
     <div>
         <p>{course.name}</p>
         {
            course.parts.map(part => <p key={part.id}>{part.name} {part.exercises}</p>) 
         }
+
+        <p><strong>total of {total} exercises</strong></p>
     </div>
+    
   )
 }
 
