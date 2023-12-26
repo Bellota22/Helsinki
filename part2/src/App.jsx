3faf6636658f17953ec89e076e31f3ee9cc4175b
Gabriel Villanueva <gvillanuevavega@gmail.com>
2023-12-25 19:01:33 -0500

    telefonic guide 2.6

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,10 +1,42 @@
-import Course from "./components/Course"
-import {courses} from "./constants"
+import React, { useState } from 'react'
+
 
 const App = () => {
-  console.log("🚀 ~ file: App.jsx:3 ~ courses:", courses)
-  
-  return <Course courses={courses} />
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+  const handleClick = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName
+    }
+    setPersons([...persons, personObject])
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: 
+          <input
+            value={newName}
+            onChange={(event) => setNewName(event.target.value)}
+          />
+        </div>
+        <div>
+          <button onClick={handleClick} type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {
+        persons.map(person => <p key={person.name}>{person.name}</p>)
+      }
+      
+    </div>
+  )
 }
 
 export default App
